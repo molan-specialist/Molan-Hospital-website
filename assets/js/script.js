6273afcb0554b0bcf39d2bd87558012685d55868
Abdulbasit Adio <abdulbasitadio@gmail.com>
2025-05-01 01:51:02 +0100

    update 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,28 +24,28 @@ window.addEventListener('scroll', function () {
   
 
 // Typing Effect
-const dynamicText = document.querySelector("h1 span");
-const words = ["Outstanding", "Exceptional", "Unique"];
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
-    // Condition....
-    if (!isDeleting && charIndex < currentWord.length) {
-        charIndex++;
-        setTimeout(typeEffect, 200);
-    } else if (isDeleting && charIndex > 0) {
-        charIndex--;
-        setTimeout(typeEffect, 100);
-    } else {
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
-    }
-}
-typeEffect();
+// const dynamicText = document.querySelector("h1 span");
+// const words = ["Outstanding", "Exceptional", "Unique"];
+// let wordIndex = 0;
+// let charIndex = 0;
+// let isDeleting = false;
+// const typeEffect = () => {
+//     const currentWord = words[wordIndex];
+//     const currentChar = currentWord.substring(0, charIndex);
+//     dynamicText.textContent = currentChar;
+//     dynamicText.classList.add("stop-blinking");
+//     // Condition....
+//     if (!isDeleting && charIndex < currentWord.length) {
+//         charIndex++;
+//         setTimeout(typeEffect, 200);
+//     } else if (isDeleting && charIndex > 0) {
+//         charIndex--;
+//         setTimeout(typeEffect, 100);
+//     } else {
+//         isDeleting = !isDeleting;
+//         dynamicText.classList.remove("stop-blinking");
+//         wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+//         setTimeout(typeEffect, 1200);
+//     }
+// }
+// typeEffect();
